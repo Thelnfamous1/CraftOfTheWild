87c05b857f4a9fe6bc2bd510c23227927bb2ff45
Thelnfamous1 <junior.garay1999@gmail.com>
2024-01-20 01:06:00 -0600

    - Added mod requirements to forge toml file. - Marked RayTracingMixin as Pseudo to not make it required.

--- Forge/src/main/java/com/Thelnfamous1/craft_of_the_wild/mixin/RayTracingMixin.java
+++ Forge/src/main/java/com/Thelnfamous1/craft_of_the_wild/mixin/RayTracingMixin.java
@@ -4,11 +4,13 @@ import com.Thelnfamous1.craft_of_the_wild.Constants;
 import net.minecraft.world.entity.Entity;
 import net.minecraftforge.entity.PartEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import snownee.jade.overlay.RayTracing;
 
+@Pseudo
 @Mixin(value = RayTracing.class, remap = false)
 public class RayTracingMixin {
 
