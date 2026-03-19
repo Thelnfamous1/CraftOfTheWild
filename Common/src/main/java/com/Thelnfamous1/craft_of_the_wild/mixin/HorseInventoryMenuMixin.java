fdb510fe030936e971e6484129fac4edfc9a76e1
Thelnfamous1 <junior.garay1999@gmail.com>
2026-03-19 14:46:08 -0500

    Fixed HorseInventoryMenu not adding slots for chested horses at the correct indices

--- Common/src/main/java/com/Thelnfamous1/craft_of_the_wild/mixin/HorseInventoryMenuMixin.java
+++ Common/src/main/java/com/Thelnfamous1/craft_of_the_wild/mixin/HorseInventoryMenuMixin.java
@@ -1,9 +1,10 @@
 package com.Thelnfamous1.craft_of_the_wild.mixin;
 
 import com.Thelnfamous1.craft_of_the_wild.duck.ChestableHorse;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.world.Container;
 import net.minecraft.world.entity.animal.horse.AbstractHorse;
-import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.HorseInventoryMenu;
 import net.minecraft.world.inventory.MenuType;
@@ -14,26 +15,29 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(HorseInventoryMenu.class)
 public abstract class HorseInventoryMenuMixin extends AbstractContainerMenu {
     @Shadow @Final private AbstractHorse horse;
 
+    @Shadow @Final private Container horseContainer;
+
     protected HorseInventoryMenuMixin(@Nullable MenuType<?> $$0, int $$1) {
         super($$0, $$1);
     }
 
-    @Inject(method = "<init>", at = @At("TAIL"))
-    private void post_init(int $$0, Inventory $$1, Container pContainer, AbstractHorse $$3, CallbackInfo ci){
+    @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/inventory/HorseInventoryMenu;hasChest(Lnet/minecraft/world/entity/animal/horse/AbstractHorse;)Z"))
+    private boolean post_init(HorseInventoryMenu instance, AbstractHorse pHorse, Operation<Boolean> original){
         if (this.craft_of_the_wild$hasChest(this.horse)) {
             for(int k = 0; k < 3; ++k) {
                 int inventoryColumns = ((ChestableHorse) this.horse).craft_of_the_wild$getInventoryColumns();
                 for(int l = 0; l < inventoryColumns; ++l) {
-                    this.addSlot(new Slot(pContainer, 2 + l + k * inventoryColumns, 80 + l * 18, 18 + k * 18));
+                    this.addSlot(new Slot(this.horseContainer, 2 + l + k * inventoryColumns, 80 + l * 18, 18 + k * 18));
                 }
             }
+            return false;
+        } else{
+            return original.call(instance, pHorse);
         }
     }
 
